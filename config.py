f123970f0c3d3e12614250996c325df93de87b3d
CestHiza <152672912+CestHiza@users.noreply.github.com>
2025-05-22 07:43:55 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-# Store your Plaid API keys here.
-# IMPORTANT: Make sure this file is listed in your .gitignore file
-# so it is NOT committed to your Git repository!
-
-PLAID_CLIENT_ID = '682efcc13d71b8002593c864'  # <--- REPLACE WITH YOUR ACTUAL PLAID CLIENT ID
-PLAID_SECRET = 'eccc02bafe7a4fe64122af50fc98e1' # <--- REPLACE WITH YOUR ACTUAL PLAID SANDBOX SECRET
-PLAID_ENV_NAME = 'sandbox' # 'sandbox', 'development', or 'production'
